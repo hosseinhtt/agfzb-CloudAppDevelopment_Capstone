37d4d8cf60be5290d8e4385300cde2479ae16838
hosseinhtt <hossein.taybi@yahoo.com>
2023-06-29 03:59:50 -0400

    models created and registered in admin inline

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,26 @@
 from django.contrib import admin
 # from .models import related models
-
+from .models import CarMake, CarModel
 
 # Register your models here.
 
+
 # CarModelInline class
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+
 
 # CarModelAdmin class
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [
+        CarModelInline,
+    ]
 
 # Register models here
+admin.site.register(CarModel)
+admin.site.register(CarMake, CarMakeAdmin)
+
+
+
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,8 +9,11 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
-
-
+class CarMake(models.Model):
+    Name = models.CharField(max_length=100)
+    Description = models.TextField()
+    def __str__(self):
+        return str(self.Name)
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
@@ -19,6 +22,22 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    Car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    Dealer_id = models.IntegerField()
+    Name = models.CharField(max_length=100)
+    TYPE_CHOICES = [
+        ('Sedan', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        # Add more choices if needed
+    ]
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    Year = models.DateField()
+    # Add any other fields you want to include in the CarModel model
+
+    def __str__(self):
+        return f"{self.Name} ({self.Car_make.Name})"
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
